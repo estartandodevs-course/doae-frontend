16b14dd0615a11b72029293318f2901fd6613471
Alan Araujo <alan@estartandodevs.com.br>
2022-11-03 23:24:02 -0300

    chore: setup redux-toolkit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './routes/error-page';
 import Root from './routes/root';
 import './index.css';
+import { store } from './store';
+import { Provider } from 'react-redux';
 import Contact from './routes/contact';
 
 const router = createBrowserRouter([
@@ -22,6 +24,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
