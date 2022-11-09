8d8da1ebbcb0e6ca752a39f6e303772478b714e3
Alan Araujo <alan@estartandodevs.com.br>
2022-11-08 22:02:00 -0300

    chore: show donations

--- src/templates/HomePage/index.jsx
+++ src/templates/HomePage/index.jsx
@@ -3,13 +3,18 @@ import Searchbar from '../../components/Searchbar';
 import { Cards } from '../../components/Cards';
 import { Collection } from '../../components/Collection';
 import { useGetInstitutionsQuery } from '../../store/institution/institution.service';
+import { useGetDonationsSumQuery } from '../../store/donation/donation.service';
+import { formatCurrency } from '../../utils/currency';
 
 const HomePage = () => {
   const { data } = useGetInstitutionsQuery();
+  const { data: sum } = useGetDonationsSumQuery(0, {
+    pollingInterval: 10000,
+  });
   return (
     <S.Wrapper>
       <Searchbar placeholder={'Pesquisar'} />
-      <Collection title={'Arrecadação em tempo real'} amount={'R$ 12.000.174,21'} />
+      <Collection title={'Arrecadação em tempo real'} amount={formatCurrency(sum)} />
       {data?.map(institution => (
         <Cards
           key={institution.id}
